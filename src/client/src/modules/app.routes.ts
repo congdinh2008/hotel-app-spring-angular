ff57a1d0593466838daf96d319876d312f49911f
Cong Dinh <congdinh2012@hotmail.com>
2024-11-27 19:41:25 +0700

    FE: Create manager layout with sidebar component

--- src/client/src/modules/app.routes.ts
+++ src/client/src/modules/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { CustomerLayoutComponent } from './shared/layouts/customer-layout/customer-layout.component';
+import { ManagerLayoutComponent } from './shared/layouts/manager-layout/manager-layout.component';
 
 export const routes: Routes = [
+    {
+        path: 'manager',
+        component: ManagerLayoutComponent
+    },
     {
         path: '',
         component: CustomerLayoutComponent,
