45e721200b85eceb5a1c7d9a9fbd931cc9380773
Van Nguyen <vannguyen2014@outlook.com>
2024-11-27 21:24:44 +0700

    BE: Implement Security Filterchain to ignore security login

---
+++ src/server/src/main/java/com/congdinh/hotelapp/configuration/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.congdinh.hotelapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/api/v1/rooms/**").anonymous()
+                        .anyRequest().anonymous())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
