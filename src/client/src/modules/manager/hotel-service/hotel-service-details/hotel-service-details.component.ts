fdbc2d553a9b8c50204d197323d59309d738b0ac
Cong Dinh <congdinh2012@hotmail.com>
2024-11-28 23:01:16 +0700

    FE: Implement Hotel Service CRUD

--- src/client/src/modules/manager/hotel-service/hotel-service-details/hotel-service-details.component.ts
+++ src/client/src/modules/manager/hotel-service/hotel-service-details/hotel-service-details.component.ts
@@ -1,12 +1,123 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  FontAwesomeModule,
+  IconDefinition,
+} from '@fortawesome/angular-fontawesome';
+import {
+  faCancel,
+  faRotateLeft,
+  faSave,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-hotel-service-details',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FontAwesomeModule],
   templateUrl: './hotel-service-details.component.html',
-  styleUrl: './hotel-service-details.component.css'
+  styleUrl: './hotel-service-details.component.css',
 })
-export class HotelServiceDetailsComponent {
+export class HotelServiceDetailsComponent implements OnInit {
+  private _selectedItem!: any;
 
+  @Input('selected-item') set selectedItem(value: any) {
+    if (value != null) {
+      this._selectedItem = value;
+      this.isEdit = true;
+      this.updatedForm();
+    } else {
+      if (this.form) {
+        this.form.reset();
+      }
+      this.isEdit = false;
+    }
+  }
+
+  get selectedItem(): any {
+    return this._selectedItem;
+  }
+
+  @Output() cancel: EventEmitter<void> = new EventEmitter<void>();
+
+  public isEdit: boolean = false;
+  private apiUrl: string = 'http://localhost:8080/api/v1/hotel-services';
+  public form!: FormGroup;
+
+  public faCancel: IconDefinition = faCancel;
+  public faSave: IconDefinition = faSave;
+  public faRotateLeft: IconDefinition = faRotateLeft;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.createForm();
+    this.updatedForm();
+  }
+  updatedForm() {
+    if (this.form && this.selectedItem) {
+      this.form.patchValue(this.selectedItem);
+    }
+  }
+
+  private createForm(): void {
+    this.form = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(255),
+      ]),
+      price: new FormControl('', [Validators.required, Validators.min(0)]),
+      active: new FormControl(true),
+    });
+  }
+
+  public onSubmit(): void {
+    // Validate form
+    if (this.form.invalid) {
+      return;
+    }
+
+    // Trich xuat du lieu tu form
+    const data = this.form.value;
+
+    if (this.isEdit) {
+      this.httpClient
+        .put(`${this.apiUrl}/${this.selectedItem.id}`, data)
+        .subscribe((result: any) => {
+          if (result) {
+            console.log('Update success');
+            this.cancel.emit();
+          } else {
+            console.log('Update failed');
+          }
+        });
+    } else {
+      // Call API to create new hotel service
+      this.httpClient.post(this.apiUrl, data).subscribe((result: any) => {
+        console.log(result);
+        // Neu thanh cong thi dong form
+        if (result != null) {
+          this.cancel.emit();
+        }
+      });
+    }
+  }
+
+  public onCancel(): void {
+    this.cancel.emit();
+  }
 }
--- src/client/src/modules/manager/hotel-service/hotel-service-list.component.ts
+++ src/client/src/modules/manager/hotel-service/hotel-service-list.component.ts
@@ -33,10 +33,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HotelServiceListComponent implements OnInit {
   public isShowDetails: boolean = false;
+  public selectedItem!: any;
+
   public faPlus: IconDefinition = faPlus;
   public faSearch: IconDefinition = faSearch;
   public faEdit: IconDefinition = faEdit;
   public faTrashCan: IconDefinition = faTrashCan;
+
   public searchForm!: FormGroup;
   public data: any[] = [];
 
@@ -66,6 +69,33 @@ export class HotelServiceListComponent implements OnInit {
       return;
     }
     this.apiUrl = `http://localhost:8080/api/v1/hotel-services/searchByHotelServiceName?keyword=${this.searchForm.value.keyword}`;
+    this.search();
+  }
+
+  public onDelete(id: string): void {
+    this.httpClient.delete(`${this.apiUrl}/${id}`).subscribe((result: any) => {
+      if (result) {
+        this.search();
+        console.log('Delete success');
+      } else {
+        console.log('Delete failed');
+      }
+    });
+  }
+
+  public onEdit(id: string): void {
+    this.isShowDetails = false;
+    this.selectedItem = this.data.find((item) => item.id === id);
+    this.isShowDetails = true;
+  }
+
+  public onCreate(): void {
+    this.isShowDetails = true;
+    this.selectedItem = null;
+  }
+
+  public cancelDetail(): void {
+    this.isShowDetails = false;
     this.search();
   }
 }
